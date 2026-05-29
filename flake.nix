8866e4abcb19f3920fa6831d4911b04a4f77840b
jpoly1219 <jpoly1219@gmail.com>
2026-05-28 23:56:43 -0700

    fix: pin home manager to master

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  home-manager = {
+    url = "github:nix-community/home-manager/master";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   };
 
   outputs = { nixpkgs, home-manager, ... }:
