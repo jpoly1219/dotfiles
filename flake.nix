52fb3c3e5e40dddbcc7abdceb21517748b37a477
jpoly1219 <jpoly1219@gmail.com>
2026-05-28 23:53:24 -0700

    fix: pin home manager and nixpkgs version to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
